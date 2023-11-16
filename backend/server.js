d7a90ffd1c40201f936025519ba0c4fb372ec500
skval55 <skvaladez@gmail.com>
2023-11-16 16:43:39 -0700

    trying new app.listen

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-// app.listen(PORT, function () {
-//   console.log(`Started on http://localhost:${PORT}`);
-// });
-app.listen(PORT, "0.0.0.0", function () {
+app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
+// app.listen(PORT, "0.0.0.0", function () {
+//   console.log(`Started on http://localhost:${PORT}`);
+// });
