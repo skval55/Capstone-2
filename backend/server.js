9bc15287eb1ab836601c6c581b17f907d67018e8
skval55 <skvaladez@gmail.com>
2023-11-16 11:42:03 -0700

    changing back

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, "groovegurubackend.vercel.app", function () {
+app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
