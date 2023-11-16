d7cfc1a83d149c221f55a2ec99c464ca8ed3f231
skval55 <skvaladez@gmail.com>
2023-11-16 12:08:03 -0700

    trying new deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-app.listen(PORT, function () {
+// app.listen(PORT, function () {
+//   console.log(`Started on http://localhost:${PORT}`);
+// });
+app.listen(PORT, "0.0.0.0", function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
