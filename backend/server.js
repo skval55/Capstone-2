26e0acb14270110e03a049aba04675928852330c
skval55 <skvaladez@gmail.com>
2023-11-16 16:38:53 -0700

    adding back server.js

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,6 @@ const { PORT } = require("./config");
 // app.listen(PORT, function () {
 //   console.log(`Started on http://localhost:${PORT}`);
 // });
-// app.listen(PORT, "0.0.0.0", function () {
-//   console.log(`Started on http://localhost:${PORT}`);
-// });
+app.listen(PORT, "0.0.0.0", function () {
+  console.log(`Started on http://localhost:${PORT}`);
+});
