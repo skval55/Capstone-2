b1dd3453843268591989d89e432fa8dbb9356df0
skval55 <skvaladez@gmail.com>
2023-11-16 11:02:43 -0700

    added http back

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const { PORT } = require("./config");
 
 app.listen(PORT, function () {
-  console.log(`Started on localhost:${PORT}`);
+  console.log(`Started on http://localhost:${PORT}`);
 });
