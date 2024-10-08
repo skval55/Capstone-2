b94828d26f13a1ae760df5016e790e58a4e4670e
Sam  Valadez <skvaladez@gmail.com>
2024-10-08 04:58:16 -0600

    cors

--- backend/routes/musicRoutes.js
+++ backend/routes/musicRoutes.js
@@ -11,7 +11,7 @@ const playlists = new Playlist();
 const setHeaders = (res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 };
 
 /** POST /auth/token:  { username, password } => { token }
